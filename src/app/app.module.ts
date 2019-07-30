ce4693ef10cbbf788dd4a6ecd4313f7c26414596
Rachit <rachit0428@gmail.com>
2019-07-30 19:19:08 +0530

    dashboard-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ModalModule } from 'ngb-modal';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardPopComponent } from './dashboard/dashboard-pop/dashboard-pop.component';
+import { DashboardHeaderComponent } from './dashboard/dashboard-header/dashboard-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DashboardPopComponent,
+    DashboardHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModalModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
